627e6173fa8a5b196bdd39f4b98b4b413e50406f
Lillys-Kosmetik-Institut <lilianapest@googlemail.com>
2026-02-09 21:31:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-script.js
+document.addEventListener("DOMContentLoaded", () => {
+    const items = document.querySelectorAll(".service-list p");
+
+    items.forEach((item, index) => {
+        item.style.opacity = "0";
+        item.style.transform = "translateY(20px)";
+        item.style.transition = "all 0.8s ease";
+
+        setTimeout(() => {
+            item.style.opacity = "1";
+            item.style.transform = "translateY(0)";
+        }, index * 120);
+    });
+});
