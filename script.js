ce38609748c170c5cf7cf28074fa4ac7883f2a45
Lillys-Kosmetik-Institut <lilianapest@googlemail.com>
2026-02-10 07:12:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-document.querySelectorAll(".service-title").forEach(button => {
-  button.addEventListener("click", () => {
-    const info = button.nextElementSibling;
+// abrir/cerrar categorías
+document.querySelectorAll(".service-cat-btn").forEach(btn => {
+  btn.addEventListener("click", () => {
+    const list = btn.nextElementSibling;
+    list.style.maxHeight = list.style.maxHeight
+      ? null
+      : list.scrollHeight + "px";
+  });
+});
+
+// abrir/cerrar info de cada tratamiento
+document.querySelectorAll(".service-title").forEach(btn => {
+  btn.addEventListener("click", () => {
+    const info = btn.nextElementSibling;
     info.style.maxHeight = info.style.maxHeight
       ? null
       : info.scrollHeight + "px";
